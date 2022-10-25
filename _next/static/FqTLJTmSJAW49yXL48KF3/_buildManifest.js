3ee3bcb2dd2e921c0c8e0ed21f065f84ec38ee5f
KentarouKoizumi <KentarouKoizumi@users.noreply.github.com>
2022-10-25 03:33:39 +0000

    deploy: 10efe8bb373ca61a4ef1d3d7af2e7565d4ff56b8

---
+++ _next/static/FqTLJTmSJAW49yXL48KF3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/618-5814c24219157103.js","static/chunks/pages/index-6b1090201d0c4c71.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
